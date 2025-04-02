459a7d97b751247b4052d04e76e2624ff71c3f81
HabibaUrmi <urmiinnah@gmail.com>
2025-04-02 20:06:03 +0300

    add Step 1 features_header, footer, and person

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from 'react'
-
+import Header from './Header';
+import Footer from './Footer';
+import Person from './Person';
 import './App.css'
 
-function App() {
 
 
-  return (
-    <>
-      
+  function App() {
+    return (
+      <div class ="app-container">
+        <Header appName ="hrApp_Step1"/>
+        <main class="person-container">
+          <Person name="Habiba Urmi" 
+          title="Web developer"
+          salary="5000"
+          phone="123-456-7890"
+          email="urmi@example.com"
+          animal="Cat" />
 
-    </>
-  )
-}
+         <Person name="Habiba Urmi" 
+          title="Web developer"
+          salary="5000"
+          phone="123-456-7890"
+          email="urmi@example.com"
+          animal="Cat" />
 
-export default App
+          <Person name="Habiba Urmi" 
+          title="Web developer"
+          salary="5000"
+          phone="123-456-7890"
+          email="urmi@example.com"
+          animal="Cat" />
+
+
+        
+        </main>
+        <Footer groupName ="REACT25K"/>
+      </div>
+    )
+  };
+  
+  export default App;
+
+
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+const Header = (props) => {
+ 
+  return (
+    <header> 
+      <h1>{props.appName}</h1>
+  
+      </header>
+   
+  );
+  };
+
+  export default Header;
